74d6c8f11374782e949a095e17f930107706275f
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2023-07-07 09:31:37 +0530

    aduio_clip

--- Assets/scripts/eyemovementNew.cs
+++ Assets/scripts/eyemovementNew.cs
@@ -9,19 +9,33 @@ public class eyemovementNew : MonoBehaviour
 
     [SerializeField] private GameObject leftEye;
     [SerializeField] private GameObject rightEye;
-    [SerializeField] private float lastScale;
+    [SerializeField] private Vector2 lastScale;
     [SerializeField] private float Sacleuptime;
+    [SerializeField] private GameObject rawImage_V_Player;
+    [SerializeField] private AudioClip welcomeClip;
+    [SerializeField] private float welcomeclip_Time;
+
+    private AudioSource audioSource;
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     void Start()
     {
-       leftEye.transform.localScale = Vector3.one;
-        rightEye.transform.localScale = Vector3.one;    
+        backToEyeZeroScale();
     }
 
-   
+    private void backToEyeZeroScale()
+    {
+        leftEye.transform.DOScale(Vector2.zero, Sacleuptime);
+        rightEye.transform.DOScale(Vector2.zero,Sacleuptime);
+    }
+
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
+            // Invoke("eyeScaleUp" , Sacleuptime);
             eyeScaleUp();
         }
     }
@@ -29,5 +43,17 @@ public class eyemovementNew : MonoBehaviour
     private void eyeScaleUp()
     {
         leftEye.transform.DOScale(lastScale, Sacleuptime);
+        rightEye.transform.DOScale(lastScale, Sacleuptime);
+      //  v_playerDesable();
+        Invoke("v_playerDesable", Sacleuptime);
+    }
+
+    private void v_playerDesable()
+    {
+        rawImage_V_Player.SetActive(false);
+        audioSource.clip = welcomeClip;
+        audioSource.Play();
+        Invoke("backToEyeZeroScale", welcomeclip_Time);
+
     }
 }
