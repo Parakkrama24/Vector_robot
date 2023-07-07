249222c931048abdf406980a5db025d0e646195d
Parakkrama Dasanayaka <110714055+Parakkrama24@users.noreply.github.com>
2023-07-07 09:09:35 +0530

    basicEyeMovement

--- Assets/scripts/eyemovementNew.cs
+++ Assets/scripts/eyemovementNew.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class eyemovementNew : MonoBehaviour
 {
-    
+
+    [SerializeField] private GameObject leftEye;
+    [SerializeField] private GameObject rightEye;
+    [SerializeField] private float lastScale;
+    [SerializeField] private float Sacleuptime;
     void Start()
     {
-        
+       leftEye.transform.localScale = Vector3.one;
+        rightEye.transform.localScale = Vector3.one;    
     }
 
    
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            eyeScaleUp();
+        }
+    }
+
+    private void eyeScaleUp()
+    {
+        leftEye.transform.DOScale(lastScale, Sacleuptime);
     }
 }
